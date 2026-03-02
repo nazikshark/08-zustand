10b49b84314a8545868a564053ccb86833b81d0d
nazikshark <nazarpro783@gmail.com>
2026-03-02 22:12:35 +0100

    fix: wrap components using useSearchParams in Suspense

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,15 +1,12 @@
+import { Suspense } from 'react';
 import App from '@/components/App/App';
-import { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Мої нотатки | NoteHub',
-  description: 'Керуй своїми ідеями та планами на майбутнє',
-};
 
 export default function NotesPage() {
   return (
     <main>
-      <App />
+      <Suspense fallback={<div>Loading...</div>}>
+        <App />
+      </Suspense>
     </main>
   );
 }
